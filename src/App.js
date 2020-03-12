23a29a918887d352cbc3e63c3d1522d849c38e3b
Mariana Carbonell <marianacarbonellm@gmail.com>
2020-03-12 12:42:11 -0300

    mostrando las card

--- src/App.js
+++ src/App.js
@@ -1,45 +1,54 @@
 import React, { Component } from 'react';
 import './App.css';
-import {todos} from './todos.json';
+import { todos } from './todos.json';
 
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       todos
     }
   }
 
-  render(){
-    this.state.todos.map((todo, i) =>{
-      return(
-        <div className="card">
-          <div className="card-header">
-            <h3>{todo.tilte}</h3>
+  render() {
+    const todos = this.state.todos.map((todo, i) => {
+      return (
+        <div className="col-md-4">
+          <div className="card mt-4">
+            <div className="card-header text-secondary">
+              <h3>{todo.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                {todo.priority}
+              </span>
+            </div>
+            <div className="card-body text-secondary">
+              <p>{todo.description}</p>
+              <p>{todo.responsible}</p>
+            </div>
           </div>
-        <div className="card-body">
-      <p>{todo.description}</p>
-
-        </div>
         </div>
       )
     })
 
 
-  return (
-    <div className="App">
-   <nav className="navbar navbar-dark bg-dark">
-            <a href="" className="text-white">
-            
-      {todos}
-            
+    return (
+      <div className="App">
+        <nav className="navbar navbar-dark bg-dark">
+          <a href="" className="text-white">
+            Tasks
             </a>
-            
-            </nav>
-    </div>
-  );
-}
+        </nav>
+        <div className="container">
+          <div className="row mt-4">
+            {todos}
+          </div>
+
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
